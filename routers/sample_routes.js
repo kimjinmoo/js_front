c9e8193ca9227741d1a46ce5915743d9ea34006c
iukim21c@gmail.com <iukim21c@gmail.com>
2017-07-09 23:38:05 +0900

    1. angular List 샘플

--- routers/sample_routes.js
+++ routers/sample_routes.js
@@ -35,4 +35,32 @@ router.get("/greeting/:language", function(req, res, next) {
     res.json(obj);
 });
 
+router.get("/list", function(req, res, next) {
+    var obj = [
+        {seq : 1, title : "테스트 글", contents : "내용1", readCount : 0, regId : "0", regDate : "2017-07-08"},
+        {seq : 2, title : "테스트 글2", contents : "내용2", readCount : 3, regId : "0", regDate : "2017-07-08"},
+        {seq : 3, title : "테스트 글3", contents : "내용3", readCount : 5, regId : "0", regDate : "2017-07-08"}
+    ];
+
+    res.json(obj);
+});
+router.get("/list/:seq", function(req, res, next) {
+    var obj = {};
+    console.log("params : " + req.params.seq);
+    switch (req.params.seq) {
+        case "1":
+            obj = {seq : 1, title : "테스트 글", contents : "내용1", readCount : 0, regId : "0", regDate : "2017-07-08"};
+            break;
+        case "2" :
+            obj = {seq : 2, title : "테스트 글2", contents : "내용2", readCount : 3, regId : "0", regDate : "2017-07-08"};
+            break;
+        case "3" :
+            obj =  {seq : 3, title : "테스트 글3", contents : "내용3", readCount : 5, regId : "0", regDate : "2017-07-08"};
+            break;
+        default :
+            obj = {};
+    }
+    res.json(obj);
+});
+
 module.exports = router;
